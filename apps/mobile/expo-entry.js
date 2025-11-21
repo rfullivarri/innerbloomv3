722af1c9dbfa4d80714e5327a390d5c4a58f3ffd
rfullivarri <113726007+rfullivarri@users.noreply.github.com>
2025-11-21 13:56:33 +0100

    Version Movil Xcode

--- apps/mobile/expo-entry.js
+++ apps/mobile/expo-entry.js
@@ -1,5 +1,7 @@
+// apps/mobile/expo-entry.js
+import 'expo-dev-client';
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+import App from './App'; // este es tu App.tsx raíz
 
 registerRootComponent(App);
