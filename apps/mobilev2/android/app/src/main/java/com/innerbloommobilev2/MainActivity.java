d7bccb8fdf37ae223f4c4e026cfe0e610a782d88
rfullivarri <113726007+rfullivarri@users.noreply.github.com>
2025-11-26 00:01:04 +0100

    Add native scaffolding for mobilev2 app

---
+++ apps/mobilev2/android/app/src/main/java/com/innerbloommobilev2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.innerbloommobilev2;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+  @Override
+  protected String getMainComponentName() {
+    return "InnerbloomMobileV2";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this, getMainComponentName(), DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+}
